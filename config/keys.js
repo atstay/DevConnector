e9f12670a6cd472185724c2d65bd9efb5cbc5d6e
Shubalub Kingz <atstay@gmail.com>
2018-12-16 20:09:12 -0700

    first

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://test:Dumbledoor1@ds139685.mlab.com:39685/devconnector2019"
+};
---
+++ routes/api/profile.js
@@ -0,0 +1,10 @@
+// fetch and create user profile (location/bio/experiences/education/social network links)
+const express = require("express");
+const router = express.Router();
+
+// @route   GET api/profile/test
+// @desc    Tests profile route
+// @access  Public
+router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
+
+module.exports = router;
