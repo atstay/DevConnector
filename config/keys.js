abb29f1918fa2f01097ac0894fc2c646e00f774f
Shubalub Kingz <atstay@gmail.com>
2018-12-18 23:45:45 -0700

    sec3

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://test:Dumbledoor1@ds139685.mlab.com:39685/devconnector2019"
+    "mongodb://test:Dumbledoor1@ds139685.mlab.com:39685/devconnector2019",
+  secretOrKey: "secret"
 };
